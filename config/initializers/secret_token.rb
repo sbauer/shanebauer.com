b02ca86410818bbaebd3c8920bdbecdf93f7519c
Shane Bauer <shane@shanebauer.com>
2012-05-31 17:10:54 -0400

    Import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bauer::Application.config.secret_token = '2d2897b30565ccad9bdaff519bc17163b01fd7b86623d6f6e58ac62556bd8690b55d3a2bc16e94e743d514409141f528beaa3de981b790eb170e095dd2085073'
